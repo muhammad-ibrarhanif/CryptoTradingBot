3aba5a484df2921009a11f60b09d0b20c6642c4f
muhammad-ibrarhanif <ibrar_hanif@live.co.uk>
2026-04-28 22:07:02 +0500

    test

---
+++ TradingBot.Backtester/BinanceDataLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Binance.Net.Clients;
+using Binance.Net.Enums;
+using global::TradingBot.Core.Models;
+using TradingBot.Core.Models;
+
+namespace TradingBot.Backtester
+{
+
+
+    public static class BinanceDataLoader
+    {
+        public static async Task<List<Candle>> FetchKlinesAsync(
+            string symbol,
+            KlineInterval interval,
+            DateTime startTime,
+            DateTime endTime)
+        {
+            using var restClient = new BinanceRestClient();
+            var klineResult = await restClient.SpotApi.ExchangeData.GetKlinesAsync(
+                symbol,
+                interval,
+                startTime,
+                endTime);
+
+            if (!klineResult.Success)
+                throw new Exception($"Failed to fetch data: {klineResult.Error}");
+
+            return klineResult.Data.Select(k => new Candle
+            {
+                OpenTime = k.OpenTime,
+                Open = k.OpenPrice,
+                High = k.HighPrice,
+                Low = k.LowPrice,
+                Close = k.ClosePrice,
+                Volume = k.Volume,
+                CloseTime = k.CloseTime
+            }).ToList();
+        }
+    }
+}
--- TradingBot.Backtester/Program.cs
+++ TradingBot.Backtester/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Binance.Net.Enums;
+using TradingBot.Backtester;
+
+Console.WriteLine("Starting backtest...");
+
+// Example: Fetch 7 days of 1‑minute data and 7 days of 1‑hour data
+var start = DateTime.UtcNow.AddDays(-7);
+var end = DateTime.UtcNow;
+
+var oneMinCandles = await BinanceDataLoader.FetchKlinesAsync("SOLUSDT", KlineInterval.OneMinute, start, end);
+var oneHourCandles = await BinanceDataLoader.FetchKlinesAsync("SOLUSDT", KlineInterval.OneHour, start, end);
+
+Console.WriteLine($"Fetched {oneMinCandles.Count} 1m candles and {oneHourCandles.Count} 1h candles.");
+
+// TODO: Run the detectors on the 1h candles to build structure/zones
+// TODO: Walk through 1m candles, check for entry signals, simulate trades
+// TODO: Print performance metrics
+
+Console.WriteLine("Backtest complete.");
